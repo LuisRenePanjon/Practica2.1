dcb66b0f64a27796e7054fa6ed8c9e0b6628bec9
Eduardo <Eduardo@DESKTOP-S4U60G8>
2017-12-01 05:10:07 -0500

    se ha creado el EventoVentanaArchivo y EventoVentanaDirectorio

--- src/Controlador/EventoVentanaArchivo.java
+++ src/Controlador/EventoVentanaArchivo.java
@@ -5,10 +5,29 @@
  */
 package Controlador;
 
+import Vista.VentanaArchivo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author SVAIO
  */
-public class EventoVentanaArchivo {
+public class EventoVentanaArchivo implements ActionListener {
+    private VentanaArchivo ventana;
+    private GestionDato gd;
+
+    public EventoVentanaArchivo(VentanaArchivo ventana) {
+        this.ventana = ventana;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+       if(e.getSource().equals(this.ventana.getBotonList().get(0))){
+           
+       }
+    }
+    
+    
     
 }
