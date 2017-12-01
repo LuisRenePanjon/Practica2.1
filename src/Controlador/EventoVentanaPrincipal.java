5cd471b1c5c0afe7156261558573d9cc7720e7cb
Eduardo <Eduardo@DESKTOP-S4U60G8>
2017-11-30 19:37:31 -0500

    se a creado la clase EventoPrincipal

--- src/Controlador/EventoVentanaPrincipal.java
+++ src/Controlador/EventoVentanaPrincipal.java
@@ -5,10 +5,35 @@
  */
 package Controlador;
 
+import Vista.VentanaPrincipal;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author SVAIO
  */
-public class EventoVentanaPrincipal {
+public class EventoVentanaPrincipal implements ActionListener {
+    private VentanaPrincipal ventana;
+    private GestionDato gd;
+
+    public EventoVentanaPrincipal(VentanaPrincipal ventana) {
+        this.ventana = ventana;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(this.ventana.getBotonList().get(0))){
+            
+        }
+        if(e.getSource().equals(this.ventana.getBotonList().get(1))){
+            
+        }
+        
+        if(e.getSource().equals(this.ventana.getBotonList().get(2))){
+            
+        }
+    }
+    
     
 }
